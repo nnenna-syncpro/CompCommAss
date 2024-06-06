36e080e07112b33c53d2da9a7f147592bd2c2b04
esot0001 <esot0001@algonquinlive.com>
2024-06-06 19:12:39 -0400

    Created UserData and ActiveUser components

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- ActiveUser should output a username (h2) and age (h3) -->
+    <ul>
+        <li><strong><h2>Username:</h2></strong> {{username}}</li>
+        <li><h3>Age:</h3> {{age}}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ["username", "age"]
+    }
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- UserData should output two input fields => for name and age -->
+    <div>
+        <label for="">Name</label>
+        <input type="text" v-model="enteredName"> 
+    </div>
+    <div>
+        <label for="">Age</label>
+        <input type="number" v-model="enteredAge">
+    </div>
+    <div>
+        <button>Add User</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        emits: ["add-user"],
+        data () {
+            return {
+                enteredName: "",
+                enteredAge: "",
+            };
+        },
+        methods: {
+            submitUser(){
+                //emit add-user so that I can forward user data 
+                this.$emit('add-user', this.enteredName, this.enteredAge);
+            }
+        }
+    }
+</script>
